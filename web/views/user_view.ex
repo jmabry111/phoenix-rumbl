2cbc5eb8ab33c67e0d6950cb1daaa44d28cd33c1
Jason Mabry <jasonmabry@JMBP.local>
2016-05-20 13:03:30 -0400

    initial commit

---
+++ web/views/user_view.ex
@@ -0,0 +1,15 @@
+defmodule Rumbl.UserView do 
+  use Rumbl.Web, :view 
+  alias Rumbl.User
+  
+  def first_name(%User{name: name}) do 
+    name
+    |> String.split(" ")
+    |> Enum.at(0)
+  end 
+  def last_name(%User{name: name}) do
+    name
+    |> String.split(" ")
+    |> Enum.at(1)
+  end
+end
