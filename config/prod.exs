503b5fcca65806f592e65230041f670f487c7f32
Jason Mabry <jmabry@mymabry.com>
2016-05-25 10:27:16 -0400

    fix typo in prod.exs

--- config/prod.exs
+++ config/prod.exs
@@ -18,7 +18,8 @@ config :rumbl, Rumbl.Endpoint,
   cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
-config: :rumbl, Rumbl.Repo
+
+config :rumbl, Rumbl.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
   pool_size: 20,
