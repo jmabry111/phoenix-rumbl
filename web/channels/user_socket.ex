2efe7007d9502ed38406e5b6c93cc2bffee25e3a
Jason Mabry <jmabry@mymabry.com>
2016-05-25 09:59:15 -0400

    Heroku env & prod setup, decrease socket timeout

--- web/channels/user_socket.ex
+++ web/channels/user_socket.ex
@@ -5,7 +5,8 @@ defmodule Rumbl.UserSocket do
   channel "videos:*", Rumbl.VideoChannel
 
   ## Transports
-  transport :websocket, Phoenix.Transports.WebSocket
+  transport :websocket, Phoenix.Transports.WebSocket,
+    timeout: 45_000
   # transport :longpoll, Phoenix.Transports.LongPoll
 
   @max_age 2 * 7 * 24 * 60 * 60
